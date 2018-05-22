920879f1ce8860362b146b0c868eab79086ba92d
Alper Kilci <alperkilci@gmail.com>
2018-05-22 22:56:27 +0200

    [fix] remove chalk

--- frontend/build/build.js
+++ frontend/build/build.js
@@ -6,7 +6,6 @@ process.env.NODE_ENV = 'production'
 const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
-const chalk = require('chalk')
 const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
@@ -28,14 +27,14 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }) + '\n\n')
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log('  Build failed with errors.\n')
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
+    console.log('  Build complete.\n')
+    console.log(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
-    ))
+    )
   })
 })
