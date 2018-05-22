b369974e44d17c3deace87b5b2de591bffc2d4ee
Alper Kilci <alperkilci@gmail.com>
2018-05-22 23:01:03 +0200

    [fix] remove chalk from frontend

--- frontend/build/check-versions.js
+++ frontend/build/check-versions.js
@@ -1,5 +1,4 @@
 'use strict'
-const chalk = require('chalk')
 const semver = require('semver')
 const packageConfig = require('../package.json')
 const shell = require('shelljs')
@@ -32,15 +31,15 @@ module.exports = function () {
 
     if (!semver.satisfies(mod.currentVersion, mod.versionRequirement)) {
       warnings.push(mod.name + ': ' +
-        chalk.red(mod.currentVersion) + ' should be ' +
-        chalk.green(mod.versionRequirement)
+        mod.currentVersion + ' should be ' +
+        mod.versionRequirement
       )
     }
   }
 
   if (warnings.length) {
     console.log('')
-    console.log(chalk.yellow('To use this template, you must update following to modules:'))
+    console.log('To use this template, you must update following to modules:')
     console.log()
 
     for (let i = 0; i < warnings.length; i++) {
